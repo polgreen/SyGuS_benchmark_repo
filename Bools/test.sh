d55f6ba348632e0ae8f3b7dc570298d4180a7149
polgreen <elizabeth.polgreen@cs.ox.ac.uk>
2020-03-10 18:32:51 +0000

    rename files

--- Bools/test.sh
+++
@@ -1,9 +0,0 @@
-for f in *.sl; do
-echo "trying $f"
-timeout 10 cvc4 --cegqi-si-abort $f > output.txt
-if grep -q "not handled by single invocation" output.txt; then
-  mv $f non_single_invocation/
-else
- mv $f single_invocation/
-fi
-done
